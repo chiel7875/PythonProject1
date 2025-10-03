20567e5070f206305eefd94654b215e4422f68e1
Fatima Yunis <chiel7875@gmail.com>
2025-10-03 23:45:59 +0300

    first commit

---
+++ prime number finder.py
@@ -0,0 +1,17 @@
+N = 0
+
+
+while N <= 3:
+    while True:
+        users_input = input("Enter your name: ")
+        if users_input in users_input == "ME":
+            print("your username is:", users_input)
+        else:
+            print("error")
+        passwords_input = input("Enter your password: ")
+        if passwords_input in passwords_input == "wtvr":
+            print("your password is:", passwords_input)
+        else:
+            print("error")
+N = N + 1
+
